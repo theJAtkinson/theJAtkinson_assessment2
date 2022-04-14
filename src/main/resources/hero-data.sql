20a3e4bc15ebc92fb31cc73f3659a40cc961eb75
Jonah Atkinson <theJAtkinson@gmail.com>
2022-04-14 15:15:45 +0100

    added tests to 80%+

--- src/main/resources/hero-data.sql
+++ src/main/resources/hero-data.sql
@@ -1,6 +1,4 @@
-INSERT INTO hero
-(name, power_level, powers, tragic_backstory) 
-VALUES
-("Batman", 10, "He's just rich and smart", "parents died in a robbery gone wrong"),
-("John Smith", 0, "None", "Stubbed toe this morning"),
-("Cool-Awesome-man", 10000, "Super speed, super strength, can control time", "Entire family died in tragic blimp accident");
+insert into hero (name, power_level, powers, tragic_backstory) values 
+('Batman',10,'He is just rich and smart','parents died in a robbery gone wrong'),
+('John Smith', 0, 'None', 'Stubbed toe this morning'),
+('Cool-Awesome-man', 10000, 'Super speed, super strength, can control time', 'Entire family died in tragic blimp accident');
