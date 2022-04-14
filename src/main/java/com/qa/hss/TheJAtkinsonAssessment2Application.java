0c4ae15393bd9148f1c05b02ad2f2fe46fe7f9c8
Jonah Atkinson <theJAtkinson@gmail.com>
2022-04-14 11:33:28 +0100

    Initial setup

---
+++ src/main/java/com/qa/hss/TheJAtkinsonAssessment2Application.java
@@ -0,0 +1,13 @@
+package com.qa.hss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheJAtkinsonAssessment2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheJAtkinsonAssessment2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/qa/hss/TheJAtkinsonAssessment2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.hss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheJAtkinsonAssessment2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
