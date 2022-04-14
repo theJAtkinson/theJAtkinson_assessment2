08dbbbb12dfa2f1c0a74a0609d421fe6db36db68
Jonah Atkinson <theJAtkinson@gmail.com>
2022-04-14 11:45:26 +0100

    added domain

---
+++ src/main/java/com/qa/hss/domain/Hero.java
@@ -0,0 +1,88 @@
+package com.qa.hss.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Hero {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(nullable = false)
+	private String name;
+	
+	@Column(nullable = false)
+	private Integer powerLevel;
+	
+	@Column(nullable = false)
+	private String powers;
+	
+	@Column(nullable = false)
+	private String tragicBackstory;
+
+	public Hero(Integer id, String name, Integer powerLevel, String powers, String tragicBackstory) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.powerLevel = powerLevel;
+		this.powers = powers;
+		this.tragicBackstory = tragicBackstory;
+	}
+
+	public Hero() {
+		super();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getPowerLevel() {
+		return powerLevel;
+	}
+
+	public void setPowerLevel(Integer powerLevel) {
+		this.powerLevel = powerLevel;
+	}
+
+	public String getPowers() {
+		return powers;
+	}
+
+	public void setPowers(String powers) {
+		this.powers = powers;
+	}
+
+	public String getTragicBackstory() {
+		return tragicBackstory;
+	}
+
+	public void setTragicBackstory(String tragicBackstory) {
+		this.tragicBackstory = tragicBackstory;
+	}
+
+	@Override
+	public String toString() {
+		return "Hero [id=" + id + ", name=" + name + ", powerLevel=" + powerLevel + ", powers=" + powers
+				+ ", tragicBackstory=" + tragicBackstory + "]";
+	}
+	
+	
+}
---
+++ src/main/resources/programmer-data.sql
@@ -0,0 +1,4 @@
+INSERT INTO programmer (bugs_made,lpm,name) VALUES
+(1000,1,'Dumbo Dummy'),
+(0,100,'Literally Einstein'),
+(4,40,'John Smith');
